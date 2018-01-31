78b5df539dcbc1c90c3a50ac8981de8980d78482
Alexander Senier <senier@componolit.com>
2018-01-31 07:09:23 +0100

    Do not mprotect() memory on Genode

--- runtime/gc/space/rosalloc_space.cc
+++ runtime/gc/space/rosalloc_space.cc
@@ -70,9 +70,11 @@ RosAllocSpace* RosAllocSpace::CreateFromMemMap(MemMap* mem_map, const std::strin
 
   // Protect memory beyond the starting size. MoreCore will add r/w permissions when necessory
   uint8_t* end = mem_map->Begin() + starting_size;
+#ifndef __GENODE__
   if (capacity - starting_size > 0) {
     CHECK_MEMORY_CALL(mprotect, (end, capacity - starting_size, PROT_NONE), name);
   }
+#endif
 
   // Everything is set so record in immutable structure and leave
   uint8_t* begin = mem_map->Begin();
