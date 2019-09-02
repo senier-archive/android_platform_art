9bf434a40a34d7868f8bda2c9178959d47156064
Alexander Senier <senier@componolit.com>
2019-09-02 19:17:09 +0200

    Set test directory on Genode
    Ref. Componolit/gart#50

--- runtime/common_runtime_test.cc
+++ runtime/common_runtime_test.cc
@@ -178,6 +178,8 @@ void CommonRuntimeTestImpl::SetUpAndroidRoot() {
         root += "/out/host/linux-x86";
 #elif defined(__APPLE__)
         root += "/out/host/darwin-x86";
+#elif defined(__GENODE__)
+        root += "/out";
 #else
 #error unsupported OS
 #endif
