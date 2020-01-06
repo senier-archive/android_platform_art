167a87ec07e49a5f94e20b26674be7508b2998fe
Alexander Senier <senier@componolit.com>
2020-01-06 21:05:57 +0100

    Do not delete self on Genode

--- runtime/thread_list.cc
+++ runtime/thread_list.cc
@@ -1477,7 +1477,9 @@ void ThreadList::Unregister(Thread* self) {
     }
     // We failed to remove the thread due to a suspend request, loop and try again.
   }
+#ifndef __GENODE__
   delete self;
+#endif
 
   // Release the thread ID after the thread is finished and deleted to avoid cases where we can
   // temporarily have multiple threads with the same thread id. When this occurs, it causes
