bd79e5ca144a6138be42aa8b082c0fccdc4b9b07
Alexander Senier <senier@componolit.com>
2020-01-07 12:32:51 +0100

    Decreas default memory footprint

--- runtime/gc/heap.h
+++ runtime/gc/heap.h
@@ -130,8 +130,8 @@ class Heap {
   // If true, measure the total allocation time.
   static constexpr size_t kDefaultStartingSize = kPageSize;
   static constexpr size_t kDefaultInitialSize = 2 * MB;
-  static constexpr size_t kDefaultMaximumSize = 64 * MB;
-  static constexpr size_t kDefaultNonMovingSpaceCapacity = 64 * MB;
+  static constexpr size_t kDefaultMaximumSize = 32 * MB;
+  static constexpr size_t kDefaultNonMovingSpaceCapacity = 32 * MB;
   static constexpr size_t kDefaultMaxFree = 2 * MB;
   static constexpr size_t kDefaultMinFree = kDefaultMaxFree / 4;
   static constexpr size_t kDefaultLongPauseLogThreshold = MsToNs(5);
